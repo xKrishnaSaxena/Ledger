d9729231e0eb13e6b8ea6888523e04dfd3455d7f
Krishna Saxena <rio17105@gmail.com>
2024-08-21 08:06:00 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,227 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import nacl from "tweetnacl";
+import bs58 from "bs58";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { Keypair } from "@solana/web3.js";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [mnemonic, setMnemonic] = useState<string | null>(null);
+  const [customMnemonic, setCustomMnemonic] = useState<string>("");
+  const [usingCustomMnemonic, setUsingCustomMnemonic] =
+    useState<boolean>(false);
+  const [keyPairArray, setKeyPairArray] = useState<
+    {
+      privateKeyBase58: string;
+      publicKeyBase58: string;
+      isPublicVisible: boolean;
+      isPrivateVisible: boolean;
+    }[]
+  >([]);
+  const darkButton1 = "#e0e1dd";
+  const darkButton2 = "#778da9";
+
+  useEffect(() => {
+    if (!usingCustomMnemonic) {
+      // Generate mnemonic and seed only on the client-side
+      const mnemonicGenerated = generateMnemonic();
+      setMnemonic(mnemonicGenerated);
+    }
+  }, [usingCustomMnemonic]);
+
+  function genKeyPair(seed: Buffer) {
+    if (!mnemonic) return; // Ensure mnemonic is available
+
+    const path = `m/44'/501'/${keyPairArray.length + 1}'/0'`; // Derivation path
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+    const privateKeyBase58 = bs58.encode(secret);
+    const publicKeyBase58 = Keypair.fromSecretKey(secret).publicKey.toBase58();
+
+    setKeyPairArray([
+      ...keyPairArray,
+      {
+        privateKeyBase58,
+        publicKeyBase58,
+        isPublicVisible: false,
+        isPrivateVisible: false,
+      },
+    ]);
+  }
+
+  function deleteKeyPair(index: number) {
+    setKeyPairArray(keyPairArray.filter((_, i) => i !== index));
+  }
+
+  function deleteAllKeyPairs() {
+    setKeyPairArray([]);
+  }
+
+  function toggleVisibility(index: number, type: "public" | "private") {
+    setKeyPairArray(
+      keyPairArray.map((keyPair, i) =>
+        i === index
+          ? {
+              ...keyPair,
+              ...(type === "public"
+                ? { isPublicVisible: !keyPair.isPublicVisible }
+                : { isPrivateVisible: !keyPair.isPrivateVisible }),
+            }
+          : keyPair
+      )
+    );
+  }
+
+  function handleMnemonicChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setCustomMnemonic(e.target.value);
+  }
+
+  function handleMnemonicSubmit() {
+    if (customMnemonic.split(" ").length === 12) {
+      setMnemonic(customMnemonic);
+      setUsingCustomMnemonic(true);
+    } else {
+      alert("Please enter a valid 12-word mnemonic.");
+    }
+  }
+
+  if (!mnemonic) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
+        <div className="dot-spinner">
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
+          <div className="dot-spinner__dot"></div>
         </div>
       </div>
+    );
+  }
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+  const seed = mnemonicToSeedSync(mnemonic);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
+      <h1 className="text-3xl text-decoration-line: underline mb-5">
+        Ledger Wallet
+      </h1>
+      <div className="text-center mb-4">
+        <div className="bg-gray-200 mb-5 dark:bg-gray-700 p-2 rounded-md">
+          <p className="text-xl font-bold">Mnemonic :</p>
+          <div className="grid grid-cols-3 gap-2 mt-2">
+            {mnemonic.split(" ").map((word, index) => (
+              <div
+                key={index}
+                className="bg-gray-300 dark:bg-gray-600 p-2 rounded-md text-center"
+              >
+                {word}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          onClick={() => genKeyPair(seed)}
+          className="mt-4 text-black py-2 px-4 rounded-md hover:bg-blue-600"
+          style={{ backgroundColor: darkButton1 }}
+        >
+          Generate KeyPair
+        </button>
+
+        {keyPairArray.length < 2 ? null : (
+          <button
+            onClick={deleteAllKeyPairs}
+            className="ml-4 text-black py-2 px-4 rounded-md hover:bg-red-600"
+            style={{ backgroundColor: darkButton2 }}
+          >
+            Delete All KeyPairs
+          </button>
+        )}
+        {!usingCustomMnemonic && (
+          <div className="mt-4">
+            <input
+              type="text"
+              value={customMnemonic}
+              onChange={handleMnemonicChange}
+              placeholder="Enter your 12-word mnemonic"
+              className="p-2 border rounded-md text-black"
+            />
+            <button
+              onClick={handleMnemonicSubmit}
+              className="ml-2 py-2 px-4 rounded-md hover:bg-green-600 text-black"
+              style={{ backgroundColor: darkButton1 }}
+            >
+              Use Custom Mnemonic
+            </button>
+          </div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full max-w-4xl">
+        <h2 className="text-xl font-bold mb-2 text-center">
+          Generated Key Pairs
+        </h2>
+        <ul className="space-y-4">
+          {keyPairArray.map((keyPair, index) => (
+            <li
+              key={index}
+              className="flex flex-col items-start space-y-4 bg-gray-200 dark:bg-gray-700 p-4 rounded-md"
+            >
+              <div className="flex items-center space-x-2">
+                <button
+                  onClick={() => toggleVisibility(index, "public")}
+                  className="text-gray-600 dark:text-gray-300"
+                >
+                  {keyPair.isPublicVisible ? (
+                    <EyeSlashIcon className="w-6 h-6" />
+                  ) : (
+                    <EyeIcon className="w-6 h-6" />
+                  )}
+                </button>
+                <div className="flex-1">
+                  <div className="text-sm">
+                    <strong>Public Key:</strong>{" "}
+                    {keyPair.isPublicVisible
+                      ? keyPair.publicKeyBase58
+                      : "**********"}
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center space-x-2">
+                <button
+                  onClick={() => toggleVisibility(index, "private")}
+                  className="text-gray-600 dark:text-gray-300"
+                >
+                  {keyPair.isPrivateVisible ? (
+                    <EyeSlashIcon className="w-6 h-6" />
+                  ) : (
+                    <EyeIcon className="w-6 h-6" />
+                  )}
+                </button>
+                <div className="flex-1">
+                  <div className="text-sm mt-2">
+                    <strong>Private Key:</strong>{" "}
+                    {keyPair.isPrivateVisible
+                      ? keyPair.privateKeyBase58
+                      : "**********"}
+                  </div>
+                </div>
+              </div>
+              <button
+                onClick={() => deleteKeyPair(index)}
+                className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-    </main>
+    </div>
   );
 }
