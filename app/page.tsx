00c7ffb8d35692133346987a2ab664209ec62e54
Krishna Saxena <rio17105@gmail.com>
2024-08-21 23:05:08 +0530

    added functionailty to add sol and eth wallets

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import bs58 from "bs58";
 import { generateMnemonic, mnemonicToSeedSync } from "bip39";
 import { derivePath } from "ed25519-hd-key";
 import { Keypair } from "@solana/web3.js";
+import { keccak256 } from "js-sha3";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
@@ -12,7 +13,15 @@ export default function Home() {
   const [customMnemonic, setCustomMnemonic] = useState<string>("");
   const [usingCustomMnemonic, setUsingCustomMnemonic] =
     useState<boolean>(false);
-  const [keyPairArray, setKeyPairArray] = useState<
+  const [keySOLPairArray, setKeySOLPairArray] = useState<
+    {
+      privateKeyBase58: string;
+      publicKeyBase58: string;
+      isPublicVisible: boolean;
+      isPrivateVisible: boolean;
+    }[]
+  >([]);
+  const [keyETHPairArray, setKeyETHPairArray] = useState<
     {
       privateKeyBase58: string;
       publicKeyBase58: string;
@@ -31,17 +40,16 @@ export default function Home() {
     }
   }, [usingCustomMnemonic]);
 
-  function genKeyPair(seed: Buffer) {
+  function genSOLKeyPair(seed: Buffer) {
     if (!mnemonic) return; // Ensure mnemonic is available
-
-    const path = `m/44'/501'/${keyPairArray.length + 1}'/0'`; // Derivation path
+    const path = `m/44'/501'/${keySOLPairArray.length + 1}'/0'`; // Derivation path
     const derivedSeed = derivePath(path, seed.toString("hex")).key;
     const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
     const privateKeyBase58 = bs58.encode(secret);
     const publicKeyBase58 = Keypair.fromSecretKey(secret).publicKey.toBase58();
 
-    setKeyPairArray([
-      ...keyPairArray,
+    setKeySOLPairArray([
+      ...keySOLPairArray,
       {
         privateKeyBase58,
         publicKeyBase58,
@@ -51,17 +59,58 @@ export default function Home() {
     ]);
   }
 
-  function deleteKeyPair(index: number) {
-    setKeyPairArray(keyPairArray.filter((_, i) => i !== index));
+  function genETHKeyPair(seed: Buffer) {
+    if (!mnemonic) return; // Ensure mnemonic is available
+    const path = `m/44'/60'/${keyETHPairArray.length + 1}'/0'`; // Derivation path
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+    const publicKey = nacl.sign.keyPair.fromSeed(derivedSeed).publicKey;
+    const ethereumAddress = `0x${keccak256(publicKey).slice(-40)}`;
+    const privateKeyBase58 = bs58.encode(secret);
+    const publicKeyBase58 = ethereumAddress;
+
+    setKeyETHPairArray([
+      ...keyETHPairArray,
+      {
+        privateKeyBase58,
+        publicKeyBase58,
+        isPublicVisible: false,
+        isPrivateVisible: false,
+      },
+    ]);
   }
 
-  function deleteAllKeyPairs() {
-    setKeyPairArray([]);
+  function deleteKeySOLPair(index: number) {
+    setKeySOLPairArray(keySOLPairArray.filter((_, i) => i !== index));
+  }
+  function deleteKeyETHPair(index: number) {
+    setKeyETHPairArray(keyETHPairArray.filter((_, i) => i !== index));
   }
 
-  function toggleVisibility(index: number, type: "public" | "private") {
-    setKeyPairArray(
-      keyPairArray.map((keyPair, i) =>
+  function deleteAllKeySOLPairs() {
+    setKeySOLPairArray([]);
+  }
+  function deleteAllKeyETHPairs() {
+    setKeyETHPairArray([]);
+  }
+
+  function toggleVisibilitySOL(index: number, type: "public" | "private") {
+    setKeySOLPairArray(
+      keySOLPairArray.map((keyPair, i) =>
+        i === index
+          ? {
+              ...keyPair,
+              ...(type === "public"
+                ? { isPublicVisible: !keyPair.isPublicVisible }
+                : { isPrivateVisible: !keyPair.isPrivateVisible }),
+            }
+          : keyPair
+      )
+    );
+  }
+  function toggleVisibilityETH(index: number, type: "public" | "private") {
+    setKeyETHPairArray(
+      keyETHPairArray.map((keyPair, i) =>
         i === index
           ? {
               ...keyPair,
@@ -127,20 +176,36 @@ export default function Home() {
         </div>
 
         <button
-          onClick={() => genKeyPair(seed)}
+          onClick={() => genSOLKeyPair(seed)}
+          className="mt-4 text-black py-2 px-4 mr-4 rounded-md hover:bg-blue-600"
+          style={{ backgroundColor: darkButton1 }}
+        >
+          Generate Solana Wallet
+        </button>
+        <button
+          onClick={() => genETHKeyPair(seed)}
           className="mt-4 text-black py-2 px-4 rounded-md hover:bg-blue-600"
           style={{ backgroundColor: darkButton1 }}
         >
-          Generate KeyPair
+          Generate Ethereum Wallet
         </button>
 
-        {keyPairArray.length < 2 ? null : (
+        {keySOLPairArray.length < 2 ? null : (
           <button
-            onClick={deleteAllKeyPairs}
+            onClick={deleteAllKeySOLPairs}
             className="ml-4 text-black py-2 px-4 rounded-md hover:bg-red-600"
             style={{ backgroundColor: darkButton2 }}
           >
-            Delete All KeyPairs
+            Delete All Solana KeyPairs
+          </button>
+        )}
+        {keyETHPairArray.length < 2 ? null : (
+          <button
+            onClick={deleteAllKeyETHPairs}
+            className="ml-4 text-black py-2 px-4 rounded-md hover:bg-red-600"
+            style={{ backgroundColor: darkButton2 }}
+          >
+            Delete All Ethereum KeyPairs
           </button>
         )}
         {!usingCustomMnemonic && (
@@ -162,66 +227,134 @@ export default function Home() {
           </div>
         )}
       </div>
-      <div className="w-full max-w-4xl">
-        <h2 className="text-xl font-bold mb-2 text-center">
-          Generated Key Pairs
-        </h2>
-        <ul className="space-y-4">
-          {keyPairArray.map((keyPair, index) => (
-            <li
-              key={index}
-              className="flex flex-col items-start space-y-4 bg-gray-200 dark:bg-gray-700 p-4 rounded-md"
-            >
-              <div className="flex items-center space-x-2">
-                <button
-                  onClick={() => toggleVisibility(index, "public")}
-                  className="text-gray-600 dark:text-gray-300"
-                >
-                  {keyPair.isPublicVisible ? (
-                    <EyeSlashIcon className="w-6 h-6" />
-                  ) : (
-                    <EyeIcon className="w-6 h-6" />
-                  )}
-                </button>
-                <div className="flex-1">
-                  <div className="text-sm">
-                    <strong>Public Key:</strong>{" "}
-                    {keyPair.isPublicVisible
-                      ? keyPair.publicKeyBase58
-                      : "**********"}
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center space-x-2">
-                <button
-                  onClick={() => toggleVisibility(index, "private")}
-                  className="text-gray-600 dark:text-gray-300"
-                >
-                  {keyPair.isPrivateVisible ? (
-                    <EyeSlashIcon className="w-6 h-6" />
-                  ) : (
-                    <EyeIcon className="w-6 h-6" />
-                  )}
-                </button>
-                <div className="flex-1">
-                  <div className="text-sm mt-2">
-                    <strong>Private Key:</strong>{" "}
-                    {keyPair.isPrivateVisible
-                      ? keyPair.privateKeyBase58
-                      : "**********"}
-                  </div>
-                </div>
-              </div>
-              <button
-                onClick={() => deleteKeyPair(index)}
-                className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600"
+      {keySOLPairArray.length > 0 ? (
+        <div className="w-full max-w-4xl">
+          <h2 className="text-xl font-bold mb-2 text-center">
+            Generated Solana Key Pairs
+          </h2>
+          <ul className="space-y-4">
+            {keySOLPairArray.map((keyPair, index) => (
+              <li
+                key={index}
+                className="flex flex-col items-start space-y-4 bg-gray-200 dark:bg-gray-700 p-4 rounded-md"
               >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+                <div className="flex items-center space-x-2">
+                  <button
+                    onClick={() => toggleVisibilitySOL(index, "public")}
+                    className="text-gray-600 dark:text-gray-300"
+                  >
+                    {keyPair.isPublicVisible ? (
+                      <EyeSlashIcon className="w-6 h-6" />
+                    ) : (
+                      <EyeIcon className="w-6 h-6" />
+                    )}
+                  </button>
+                  <div className="flex-1">
+                    <div className="text-sm">
+                      <strong>Public Key:</strong>{" "}
+                      {keyPair.isPublicVisible
+                        ? keyPair.publicKeyBase58
+                        : "**********"}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <button
+                    onClick={() => toggleVisibilitySOL(index, "private")}
+                    className="text-gray-600 dark:text-gray-300"
+                  >
+                    {keyPair.isPrivateVisible ? (
+                      <EyeSlashIcon className="w-6 h-6" />
+                    ) : (
+                      <EyeIcon className="w-6 h-6" />
+                    )}
+                  </button>
+                  <div className="flex-1">
+                    <div className="text-sm mt-2">
+                      <strong>Private Key:</strong>{" "}
+                      {keyPair.isPrivateVisible
+                        ? keyPair.privateKeyBase58
+                        : "**********"}
+                    </div>
+                  </div>
+                </div>
+                <button
+                  onClick={() => deleteKeySOLPair(index)}
+                  className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
+      {keyETHPairArray.length > 0 ? (
+        <div className="w-full max-w-4xl mt-4">
+          <h2 className="text-xl font-bold mb-2 text-center">
+            Generated Ethereum Key Pairs
+          </h2>
+          <ul className="space-y-4">
+            {keyETHPairArray.map((keyPair, index) => (
+              <li
+                key={index}
+                className="flex flex-col items-start space-y-4 bg-gray-200 dark:bg-gray-700 p-4 rounded-md"
+              >
+                <div className="flex items-center space-x-2">
+                  <button
+                    onClick={() => toggleVisibilityETH(index, "public")}
+                    className="text-gray-600 dark:text-gray-300"
+                  >
+                    {keyPair.isPublicVisible ? (
+                      <EyeSlashIcon className="w-6 h-6" />
+                    ) : (
+                      <EyeIcon className="w-6 h-6" />
+                    )}
+                  </button>
+                  <div className="flex-1">
+                    <div className="text-sm">
+                      <strong>Public Key:</strong>{" "}
+                      {keyPair.isPublicVisible
+                        ? keyPair.publicKeyBase58
+                        : "**********"}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <button
+                    onClick={() => toggleVisibilityETH(index, "private")}
+                    className="text-gray-600 dark:text-gray-300"
+                  >
+                    {keyPair.isPrivateVisible ? (
+                      <EyeSlashIcon className="w-6 h-6" />
+                    ) : (
+                      <EyeIcon className="w-6 h-6" />
+                    )}
+                  </button>
+                  <div className="flex-1">
+                    <div className="text-sm mt-2">
+                      <strong>Private Key:</strong>{" "}
+                      {keyPair.isPrivateVisible
+                        ? keyPair.privateKeyBase58
+                        : "**********"}
+                    </div>
+                  </div>
+                </div>
+                <button
+                  onClick={() => deleteKeyETHPair(index)}
+                  className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
